e3bc403ee1adee45ca6228c8b1af29a2c7fbd80d
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-04 18:18:59 -0500

    Initial Styling

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import API_KEY from "./apikeys.js";
+
+fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=batman`)
+.then(response => response.json())
+.then(data => console.log(data));
 
 
-console.log(process.env.KEY);
