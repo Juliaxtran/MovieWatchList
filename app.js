a650903fa2df6ace183783c9faeace54abe83b95
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-04 18:46:22 -0500

    Submit Form

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 import API_KEY from "./apikeys.js";
+const form = document.querySelector("form");
+let searchValue;
+const searchBar = document.getElementById("search-bar");
 
-fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=batman`)
-.then(response => response.json())
-.then(data => console.log(data));
 
 
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchValue = searchBar.value;
+  searchBar.value = "";
+  fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${searchValue}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+});
