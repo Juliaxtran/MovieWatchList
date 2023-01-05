8b997a18c9df72e180031ce853a6329440deb78d
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-04 20:24:35 -0500

    Movie List Styling

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   searchValue = searchBar.value;
   searchBar.value = "";
-  fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${searchValue}`)
+  fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}`)
     .then(response => response.json())
     .then(data => console.log(data));
 
-});
+});   
